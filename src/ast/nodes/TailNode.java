f8e9ef6f351ca93cfafa79b022488859e8f6a30a
gabyzola <zolag@merrimack.edu>
2025-11-10 13:06:41 -0500

    Enhance TailNode type checking to unify with ListType for tail expressions

--- src/ast/nodes/TailNode.java
+++ src/ast/nodes/TailNode.java
@@ -5,23 +5,22 @@ import java.util.LinkedList;
 import ast.EvaluationException;
 import ast.typesystem.TypeException;
 import ast.typesystem.inferencer.Inferencer;
+import ast.typesystem.types.ListType;
 import ast.typesystem.types.Type;
+import ast.typesystem.types.VarType;
 import environment.Environment;
 import environment.TypeEnvironment;
 
-public class TailNode extends SyntaxNode
-{
+public class TailNode extends SyntaxNode {
     private final SyntaxNode expr;
 
-    public TailNode(SyntaxNode expr, long lineNumber)
-    {
+    public TailNode(SyntaxNode expr, long lineNumber) {
         super(lineNumber);
         this.expr = expr;
     }
 
     @Override
-    public Object evaluate(Environment env) throws EvaluationException
-    {
+    public Object evaluate(Environment env) throws EvaluationException {
         Object v = expr.evaluate(env);
 
         if (!(v instanceof LinkedList<?> list))
@@ -31,20 +30,24 @@ public class TailNode extends SyntaxNode
             throw new EvaluationException();
 
         LinkedList<Object> out = new LinkedList<>();
-        for (int i = 1; i < list.size(); i++) out.add(list.get(i));
+        for (int i = 1; i < list.size(); i++)
+            out.add(list.get(i));
         return out;
     }
 
     @Override
-    public Type typeOf(TypeEnvironment tenv, Inferencer inferencer) throws TypeException
-    {
-        // Conservative: return the same static type as the expression.
-        return expr.typeOf(tenv, inferencer);
+    public Type typeOf(TypeEnvironment tenv, Inferencer inferencer) throws TypeException {
+        Type exprType = expr.typeOf(tenv, inferencer);
+        VarType elemType = tenv.getTypeVariable();
+
+        inferencer.unify(exprType, new ListType(elemType), "tl expects a list");
+
+        // The tail is itself a list of the same element type.
+        return inferencer.getSubstitutions().apply(exprType);
     }
 
     @Override
-    public void displaySubtree(int indentAmt)
-    {
+    public void displaySubtree(int indentAmt) {
         printIndented("tl", indentAmt);
         expr.displaySubtree(indentAmt + 2);
     }
