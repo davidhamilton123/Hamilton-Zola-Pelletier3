283d5e7a0fd676992434816be703a40a53c24ba3
gabyzola <zolag@merrimack.edu>
2025-11-09 22:00:09 -0500

    ListType class

---
+++ src/ast/typesystem/types/ListType.java
@@ -0,0 +1,30 @@
+package ast.typesystem.types;
+
+public class ListType extends Type
+{
+    private Type elementType;
+
+    public ListType(Type elementType)
+    {
+        this.elementType = elementType;
+    }
+
+    public Type getElementType()
+    {
+        return elementType;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof ListType))
+            return false;
+        return elementType.equals(((ListType)obj).elementType);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "[" + elementType.toString() + "]";
+    }
+}
